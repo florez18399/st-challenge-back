f4ba42b7e1d2cbd71231274ae2e0f0ac4e2fdb34
andrezflorezc <andres.florez03@uptc.edu.co>
2023-01-28 13:06:57 -0500

    Initial commit

---
+++ src/main/java/com/st/challenge/StChallengeBackApplication.java
@@ -0,0 +1,13 @@
+package com.st.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StChallengeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StChallengeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/st/challenge/StChallengeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StChallengeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
